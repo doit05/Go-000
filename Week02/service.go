e7434252570cd7d5047b1bbb349bfbb406afc9df
doit <shuaibing0506@163.com>
2020-12-02 16:28:23 +0800

    Update service.go

--- Week02/service.go
+++ Week02/service.go
@@ -3,8 +3,9 @@ package services
 import "log"
 
 type usr interface {
-
+  GetUsers(userIdList []int64) ([]User, error)
 }
+
 func getUsers(uids []int64) {
   userList, err := usrInstance.GetUsers(uids)
   if err != nil {
